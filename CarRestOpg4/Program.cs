4b73d8d18523c4baafc77ac14cd276e6d0387708
olliwh <98967791+olliwh@users.noreply.github.com>
2023-03-07 10:54:27 +0100

    add swagger

--- CarRestOpg4/Program.cs
+++ CarRestOpg4/Program.cs
@@ -5,10 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddSingleton<CarsRepository>(new CarsRepository());
 
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 // Configure the HTTP request pipeline.
 
